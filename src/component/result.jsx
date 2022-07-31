8d1ce2684facf58e21f80f8964fc18b30334e9eb
Lee Sun-Hyoup <kciter@naver.com>
2022-07-31 19:28:17 +0900

    MBTI별 이미지를 적용했다.

--- src/component/result.jsx
+++ src/component/result.jsx
@@ -1,17 +1,19 @@
-import { Button } from "@co-design/core";
+import { Button, Container, Heading, Image } from "@co-design/core";
 
 const Result = ({ answers, onRetry }) => {
   const EI = answers.slice(0, 3).filter((x) => x === 0).length >= 2 ? "E" : "I";
   const NS = answers.slice(3, 6).filter((x) => x === 0).length >= 2 ? "N" : "S";
   const TF = answers.slice(6, 9).filter((x) => x === 0).length >= 2 ? "T" : "F";
-  const PJ =
-    answers.slice(9, 12).filter((x) => x === 0).length >= 2 ? "P" : "J";
+  const PJ = answers.slice(9, 12).filter((x) => x === 0).length >= 2 ? "P" : "J";
+
+  const mbti = `${EI}${NS}${TF}${PJ}`;
 
   return (
-    <>
-      {`${EI}${NS}${TF}${PJ}`}
+    <Container size="xsmall">
+      <Heading>{mbti}</Heading>
+      <Image src={`/images/${mbti}.png`} width={256} fit="contain" />
       <Button onClick={onRetry}>다시하기</Button>
-    </>
+    </Container>
   );
 };
 
